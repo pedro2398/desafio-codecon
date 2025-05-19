f000c2b4631d52a135c2938af3fcd303965f4020
pedro2398 <pedrooo2398@gmail.com>
2025-05-18 21:35:52 -0300

    chore: create endpoints descriptions

--- index.ts
+++ index.ts
@@ -43,15 +43,36 @@ app.post("/users", async (req, res) => {
   }
 });
 
-app.get("/superusers");
+app.get("/superusers", (_req, _res) => {
+  //Filtro: score >= 900 e active = true
+  //Retorna os dados e o tempo de processamento da requisição.
+});
 
-app.get("/top-countries");
+app.get("/top-countries", (_req, _res) => {
+  //Agrupa os superusuários por país.
+  //Retorna os 5 países com maior número de superusuários.
+});
 
-app.get("/team-insights");
+app.get("/team-insights", (_req, _res) => {
+  //Agrupa por team.name.
+  //Retorna: total de membros, líderes, projetos concluídos e % de membros ativos.
+});
 
-app.get("/active-users-per-day");
+app.get("/active-users-per-day", (_req, _res) => {
+  //Conta quantos logins aconteceram por data.
+  //Query param opcional: ?min=3000 para filtrar dias com pelo menos 3.000 logins.
+});
 
-app.get("/evaluation");
+app.get("/evaluation", (_req, _res) => {
+  //Ele deve executar uma autoavaliação dos principais endpoints da API e retornar um relatório de pontuação.
+
+  //A avaliação deve testar:
+
+  //Se o status retornado é 200
+  //O tempo em milisegundos de resposta
+  //Se o retorno é um JSON válido
+  //Esse endpoint pode rodar scripts de teste embutidos no próprio projeto e retornar um JSON com os resultados. Ele será utilizado para validar a entrega de forma automática e rápida.
+});
 
 app.use((_req, res) => {
   res.status(404);
