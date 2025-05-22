4c87611e1e2d66cea2ac2095ab60258900be8ff0
pedro2398 <pedrooo2398@gmail.com>
2025-05-22 04:04:47 -0300

    fix: db connection

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '../generated/prisma/client'
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@db:5432/${process.env.POSTGRES_DB}`
 
 const prismaClient = new PrismaClient({
   datasources: {
