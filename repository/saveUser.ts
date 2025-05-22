7c69107a11e004535f9af50a9cb05c9b64a469ef
pedro2398 <pedrooo2398@gmail.com>
2025-05-22 01:05:39 -0300

    fix: user id generate

--- repository/saveUser.ts
+++ repository/saveUser.ts
@@ -1,11 +1,8 @@
 import prismaClient from "../config";
 import { User } from "../utils";
 
-export const saveUser = async (user: User) => {
+export const saveUser = async (data: User) => {
   return prismaClient.user.create({
-    data: {
-      ...user,
-      id: user.id as string,
-    },
+    data,
   });
 };
--- utils/user.ts
+++ utils/user.ts
@@ -1,5 +1,5 @@
 export interface User {
-  id?: string;
+  id: string;
   name: string;
   age: number;
   score: number;
