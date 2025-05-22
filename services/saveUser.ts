56dc95af80d9913058cd1d824ef495fb0ed92eea
pedro2398 <pedrooo2398@gmail.com>
2025-05-22 03:09:10 -0300

    chore: add try catch

--- services/saveUser.ts
+++ services/saveUser.ts
@@ -2,5 +2,9 @@ import { User } from "../utils";
 import { saveUser as saveUserRepository } from '../repository'
 
 export const saveUser = async (user: User) => {
-  return await saveUserRepository(user);
+  try {
+    return await saveUserRepository(user);
+  } catch (err) {
+    console.error('Error saving user: ', err);
+  }
 };
